7f09e85720f32cecb4a64a9a4a5747ff52c28ab6
Ali Alaa <hello@alialaa.com>
2022-01-01 11:27:51 +0200

    Added number of posts and feat image options

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '206f69732df187b9ecb1f37c7362fb18');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'ee191dda09ac027d5f6c48a2e9b95386');
